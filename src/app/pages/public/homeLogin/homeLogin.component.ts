5bfad208cdc3e710f1a796963923ba7e664ccbd9
david123456858 <juandavidperaltafuentes@gmail.com>
2024-09-01 14:58:55 -0500

    rama desarrollo

---
+++ src/app/pages/public/homeLogin/homeLogin.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-login',
+  templateUrl: './homeLogin.component.html',
+  styleUrls: ['./homeLogin.component.css'],
+})
+export class HomeLoginComponent { }
---
+++ src/app/pages/public/loggin/loggin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loggin',
+  templateUrl: './loggin.component.html',
+  styleUrls: ['./loggin.component.css']
+})
+export class LogginComponent {
+
+}
