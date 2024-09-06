c9de1f9d7360124b7555ac4e6c115be66e558ad4
david123456858 <juandavidperaltafuentes@gmail.com>
2024-09-05 20:38:08 -0500

    listo alguans cosas de cables

--- src/app/pages/private/mainpage/mainpage.component.ts
+++ src/app/pages/private/mainpage/mainpage.component.ts
@@ -1,25 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { setKey } from 'src/app/service/key.dinamic';
 import { getCurrentUser } from 'src/app/service/user.data';
 
 @Component({
   selector: 'app-mainpage',
   templateUrl: './mainpage.component.html',
-  styleUrls: ['./mainpage.component.css']
+  styleUrls: ['./mainpage.component.css'],
 })
 export class MainpageComponent implements OnInit {
   Option: boolean = false;
-  
-  user:any
-  Options(value: boolean){
+  showInfoBox: boolean = false; // Controla la visibilidad del cuadro
+  dynamicCode: string = '';
+  copied: boolean = false;
+  stateKey!: boolean ;
+  user: any;
+
+
+  Options(value: boolean) {
     this.Option = value;
   }
   
-  dateUser(){
-    this.user = getCurrentUser()
-    console.log(this.user.mesagge.saldo)
+  ngOnInit(): void {
+    this.dateUser();
   }
 
-  ngOnInit(): void {
-    this.dateUser()
+  dateUser() {
+    this.user = getCurrentUser();
+    console.log(this.user.mesagge.saldo);
+    this.generateDynamicCode();
+  }
+  
+  generateDynamicCode() {
+    if(this.user.mesagge.type === 'NEQUI'){
+      this.showInfoBox = true; // Hace visible el cuadro
+    }
+    // Genera una clave aleatoria de 6 dígitos
+    this.dynamicCode = Math.floor(100000 + Math.random() * 900000).toString();
+    this.stateKey = true
+
+    setKey(this.dynamicCode)
+
+    setTimeout(() => {
+      this.showInfoBox = false; // Oculta el cuadro después de 10 segundos
+      this.generateDynamicCode()
+    }, 20000);
+  }
+
+  copyToClipboard() {
+    navigator.clipboard.writeText(this.dynamicCode).then(() => {
+      this.copied = true; // Indica que la clave fue copiada
+      setTimeout(() => (this.copied = false), 2000); // Muestra el estado de copiado por 2 segundos
+    });
   }
 }
--- src/app/pages/private/optionspage/optionspage.component.ts
+++ src/app/pages/private/optionspage/optionspage.component.ts
@@ -7,8 +7,17 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class OptionspageComponent {
   @Output() changeState = new EventEmitter<boolean>();
+  selectedAmount: number | null = null;
 
   close(){
     this.changeState.emit(false);
+  }
+
+  selectAmount(amount: number) {
+    this.selectedAmount = amount;  // Guarda la cantidad seleccionada
+  }
+
+  isActive(amount: number): boolean {
+    return this.selectedAmount === amount; // Verificamos si es el monto activo
   }
 }
